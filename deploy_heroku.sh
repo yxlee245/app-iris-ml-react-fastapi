bc312ea8d4bd5c7fe06c68fa3ae1742e6687f1db
yxlee245 <the.force.of.life@gmail.com>
2021-05-29 00:15:11 +0800

    Create shell script to deploy container into Heroku

---
+++ deploy_heroku.sh
@@ -0,0 +1,18 @@
+# Login to Heroku container registry
+echo "Logging in to Heroku Container Registry..."
+echo "$HEROKU_API_KEY" | docker login -u "$HEROKU_ID" \
+--password-stdin registry.heroku.com
+
+# Build containers
+echo "Building containers..."
+docker build -t registry.heroku.com/$APP_NAME/web .
+
+# Push containers into Heroku container registry
+echo "Pushing containers into Heroku Container Registry..."
+docker push registry.heroku.com/$APP_NAME/web
+
+# Release containers
+echo "Releasing containers..."
+heroku container:release web --app $APP_NAME
+
+echo "Deployment completed"
