3444babf2669da374421d59288e4e55d58b3c350
yxlee245 <the.force.of.life@gmail.com>
2021-05-28 18:21:55 +0800

    Set up hooks in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import React, { useState } from 'react';
 import TopAppBar from './components/TopAppBar';
 
 function App() {
+
+  const [state, setState] = useState({
+    sepalLengthCm: 5.0,
+    sepalWidthCm: 5.0,
+    petalLengthCm: 5.0,
+    petalWidthCm: 5.0
+  })
+
   return (
     <div className="App">
       <TopAppBar />
