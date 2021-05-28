dc6be3db63a4d05385bc11d3825c9ed29c1df981
yxlee245 <the.force.of.life@gmail.com>
2021-05-28 17:47:14 +0800

    Add command to activate and deactivate Python virtual environment

--- start_stop_servers.sh
+++ start_stop_servers.sh
@@ -1,10 +1,13 @@
 if [ $1 == "start" ]
 then
+    source iris-env/Scripts/activate
+    # source iris-env/bin/activate
     uvicorn main:app --reload &
     npm run start &
 elif [ $1 == "stop" ]
 then
     kill %{1,2}
+    deactivate
 else
     echo "$1 is not a valid argument. Only \"start\" and \"stop\" accepted"
 fi
